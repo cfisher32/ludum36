17d8f94b0b8a8499e33c0e119fa0fcce54b6d332
cfisher <cfisher@redlinetek.com>
2016-08-27 13:49:15 -0700

    project cleanup - working

--- Ludum36-Hunt/Assets/scripts/PlayerController.cs
+++ Ludum36-Hunt/Assets/scripts/PlayerController.cs
@@ -15,33 +15,14 @@ public class PlayerController : MonoBehaviour {
 	public Plane targetPlane;
 	public Vector3 targetPoint;
 	public Vector3 throwVector = new Vector3(0, 1, 1);
-
 	public LayerMask throwLayerMask;
-
-	void Start()
-	{
-		//targetPlane = new Plane(Vector3.up, Vector3.zero);
-		//targetPlane = new Plane(Vector3.up, transform.position);
-	}
-	
 
 	void Update()
 	{
-		//createTargetPlane();
 		if (Input.GetAxis("Fire1") > 0)
 		{
 			throwWeapon();
-			//Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			//if (Physics.Raycast(ray))
-			//{
-			//	targetPoint = ray.GetPoint(100.0f);
-			//	throwWeapon();
-			//	//Debug.DrawRay(playerThrowSpot.position, targetPoint, Color.blue);
-			//	Debug.Log(ray.GetPoint(100.0f).ToString());
-			//}
 		}
-
-		//rotateThrowingArm();
 	}
 
 	void throwWeapon()
@@ -55,21 +36,15 @@ public class PlayerController : MonoBehaviour {
 
 			if (Physics.Raycast(targetRay, out hit, 100.0f, throwLayerMask))
 			{
-				//Debug.DrawLine(transform.position, hit.point);
 				Debug.DrawRay(playerThrowSpot.position, hit.point, Color.blue);
 
 				GameObject bullet = (GameObject)Instantiate(projectile, playerThrowSpot.position, Quaternion.identity);
-				//playerThrowSpot.LookAt(hit.point);
 				bullet.transform.LookAt(hit.point);
 
-				//Vector3 throwVector = new Vector3(0, 1, 1);
 				Rigidbody bulletRB = bullet.GetComponent<Rigidbody>();
-				//bulletRB.velocity = bullet.transform.forward * 10;
 				bulletRB.AddForce((hit.point - bullet.transform.position) * throwSpeed);
 
 				Physics.IgnoreCollision(bullet.GetComponent<Collider>(), GetComponent<Collider>());
-
-				//GameObject ammo = (GameObject)Instantiate(projectile, playerThrowSpot.position, Quaternion.identity);
 			}
 		}
 	}
--- Ludum36-Hunt/Assets/scripts/ProjectileController.cs
+++ Ludum36-Hunt/Assets/scripts/ProjectileController.cs
@@ -7,17 +7,11 @@ public class ProjectileController : MonoBehaviour
 {
 	//mass, drag per weapon
 
-	public float throwSpeed = 100.0f;
-	public Vector3 throwVector = new Vector3(0,1,1);
-
 	Rigidbody myRB;
 
 	void Awake()
 	{
-		throwVector = transform.forward;
 		myRB = GetComponent<Rigidbody>();
-
-		//myRB.AddForce(throwVector * throwSpeed);
 	}
 
 	void OnCollisionEnter(Collision other)
